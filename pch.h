e749e1cbf9f4df5055defc0e08cbaef0b654c9cd
Ilgiz <ii01123581321@gmail.com>
2021-05-04 22:35:44 +0500

    Added files with tests for BinarySearch class

---
+++ pch.h
@@ -0,0 +1,13 @@
+//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include "../algorithms/BinarySearch.h"
+#include <vector>
+#include <string>
+#include <deque>
+#include <array>
+#include "gtest/gtest.h"
