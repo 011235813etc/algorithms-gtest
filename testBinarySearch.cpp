43f508dbc0d65fe4b1278b5671fa833255606b48
Ilgiz <ii01123581321@gmail.com>
2021-05-12 22:24:48 +0500

    Added tests for recursive binary search

--- testBinarySearch.cpp
+++ testBinarySearch.cpp
@@ -3,23 +3,61 @@
 TEST(TestCaseName, TestBinarySearchVectorInt) {
 	std::vector<int> vec{ 0, 11, 222, 333, 444, 555, 666, 777, 888, 999 };
 	BinarySearch<std::vector<int>, int> binary_search(vec, vec.size());
-	EXPECT_EQ(9, binary_search.find(999));
+	EXPECT_EQ(9, binary_search.search(999));
 }
 
 TEST(TestCaseName, TestBinarySearchVectorString) {
 	std::vector<std::string> vec{ "aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "qqq", "rrr", "sss" };
 	BinarySearch<std::vector<std::string>, std::string> binary_search(vec, vec.size());
-	EXPECT_EQ(5, binary_search.find("fff"));
+	EXPECT_EQ(5, binary_search.search("fff"));
 }
 
 TEST(TestCaseName, TestBinarySearchDequeInt) {
 	std::deque<int> deq{ 0, 11, 222, 333, 444, 555, 666, 777, 888, 999 };
 	BinarySearch<std::deque<int>, int> binary_search(deq, deq.size());
-	EXPECT_EQ(0, binary_search.find(0));
+	EXPECT_EQ(0, binary_search.search(0));
 }
 
 TEST(TestCaseName, TestBinarySearchArrayInt) {
 	std::array<int, 10> arr{ 0, 11, 222, 333, 444, 555, 666, 777, 888, 999 };
 	BinarySearch<std::array<int, 10>, int> binary_search(arr, arr.size());
-	EXPECT_EQ(2, binary_search.find(222));
+	EXPECT_EQ(2, binary_search.search(222));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+TEST(TestCaseName, TestRecursiveBinarySearchVectorInt) {
+	std::vector<int> vec{ 0, 11, 222, 333, 444, 555, 666, 777, 888, 999 };
+	BinarySearch<std::vector<int>, int> binary_search(vec, vec.size());
+	EXPECT_EQ(9, binary_search.search_recursive(999));
+}
+
+TEST(TestCaseName, TestRecursiveBinarySearchVectorString) {
+	std::vector<std::string> vec{ "aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "qqq", "rrr", "sss" };
+	BinarySearch<std::vector<std::string>, std::string> binary_search(vec, vec.size());
+	EXPECT_EQ(5, binary_search.search_recursive("fff"));
+}
+
+TEST(TestCaseName, TestRecursiveBinarySearchDequeInt) {
+	std::deque<int> deq{ 0, 11, 222, 333, 444, 555, 666, 777, 888, 999 };
+	BinarySearch<std::deque<int>, int> binary_search(deq, deq.size());
+	EXPECT_EQ(0, binary_search.search_recursive(0));
+}
+
+TEST(TestCaseName, TestRecursiveBinarySearchArrayInt) {
+	std::array<int, 10> arr{ 0, 11, 222, 333, 444, 555, 666, 777, 888, 999 };
+	BinarySearch<std::array<int, 10>, int> binary_search(arr, arr.size());
+	EXPECT_EQ(2, binary_search.search_recursive(222));
 }
